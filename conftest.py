884edfb3436033d3970e1ee28ab51c5bb35da2a8
evgenii klimashko <klimashko108@gmail.com>
2023-05-30 23:46:15 +0300

    Изменил  версию python pyproject.toml на 3.10.5

--- conftest.py
+++ conftest.py
@@ -1,8 +1,8 @@
 import os
 import pytest
 from dotenv import load_dotenv
-
 from utils.helper import BaseSession
+
 
 load_dotenv()
 
@@ -11,9 +11,10 @@ BOOKER_URL = os.getenv('booker_base_url')
 
 
 @pytest.fixture(scope="session")
-def booker():
+def booker(allure_attachments_flag=1):
     with BaseSession(base_url=BOOKER_URL) as session:
         yield session
+
 
 @pytest.fixture(scope="session")  # это для примера
 def reqres():
