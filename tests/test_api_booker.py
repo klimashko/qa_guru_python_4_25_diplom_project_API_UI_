e242442cc4c82459a5f548f5c877d5e3116c260a
evgenii klimashko <klimashko108@gmail.com>
2023-06-03 03:37:43 +0300

    Добавил allure степы в тест test_api_booker.py

--- tests/test_api_booker.py
+++ tests/test_api_booker.py
@@ -3,6 +3,7 @@ from pytest_voluptuous import S
 from requests import Response
 import allure
 from allure_commons.types import Severity
+from allure import step
 from models.api_model import BookingData
 from schemas.booker import auth_create_token, create_new_booking, get_booking_ids, get_booking
 from utils.functions_creates_api_data import create_new_bookingid
@@ -19,13 +20,14 @@ load_dotenv()
 @allure.feature('A simple health check endpoint to confirm whether the API is up and running')
 @allure.story('Booking')
 @allure.severity(Severity.BLOCKER)
-def test_ping_health_check_api(booker):
+def test_ping_health_check(booker):
     """A simple health check endpoint to confirm whether the API is up and running"""
+    with step("Response getting"):
+        response: Response = booker.get('/ping')
 
-    response: Response = booker.get('/ping')
-
-    assert response.status_code == 201
-    assert response.text == 'Created'
+    with step("Сheck results"):
+        assert response.status_code == 201
+        assert response.text == 'Created'
 
 
 @allure.tag("api")
@@ -36,13 +38,16 @@ def test_ping_health_check_api(booker):
 def test_auth_create_token(booker):
     """Creates a new auth token to use for access to the PUT and DELETE /booking"""
 
-    payload = BookingData.payload_data_create_token()
+    with step("Data preparation"):
+        payload = BookingData.payload_data_create_token()
 
-    response: Response = booker.post('/auth', data=payload)
+    with step("Response getting"):
+        response: Response = booker.post('/auth', data=payload)
 
-    assert response.status_code == 200
-    assert S(auth_create_token.schema) == response.json()
-    assert response.json()["token"].isalnum()
+    with step("Сheck results"):
+        assert response.status_code == 200
+        assert S(auth_create_token.schema) == response.json()
+        assert response.json()["token"].isalnum()
 
 
 @allure.tag("api")
@@ -53,17 +58,20 @@ def test_auth_create_token(booker):
 def test_create_new_booking(booker):
     """Creates a new booking in the API"""
 
-    headers = BookingData.headers_data()
-    payload = BookingData.payload_data()
+    with step("Data preparation"):
+        headers = BookingData.headers_data()
+        payload = BookingData.payload_data()
+        data = json.dumps(payload)
 
-    data = json.dumps(payload)
-    response: Response = booker.post('/booking', headers=headers, data=data)
+    with step("Response getting"):
+        response: Response = booker.post('/booking', headers=headers, data=data)
 
-    assert response.status_code == 200
-    assert S(create_new_booking.schema) == response.json()
-    assert payload.get("firstname") == response.json()["booking"]["firstname"]
-    assert payload.get("lastname") == response.json()["booking"]["lastname"]
-    assert payload.get("depositpaid") == response.json()["booking"]["depositpaid"]
+    with step("Сheck results"):
+        assert response.status_code == 200
+        assert S(create_new_booking.schema) == response.json()
+        assert payload.get("firstname") == response.json()["booking"]["firstname"]
+        assert payload.get("lastname") == response.json()["booking"]["lastname"]
+        assert payload.get("depositpaid") == response.json()["booking"]["depositpaid"]
 
 
 @allure.tag("api")
@@ -88,11 +96,15 @@ def test_get_booking_ids(booker):
 def test_get_booking(booker):
     """Returns a specific booking based upon the booking id provided"""
 
-    id = create_new_bookingid(booker)
-    response: Response = booker.get(f'/booking/{id}')
+    with step("Data preparation"):
+        id = create_new_bookingid(booker)
 
-    assert response.status_code == 200
-    assert S(get_booking.schema) == response.json()
+    with step("Response getting"):
+        response: Response = booker.get(f'/booking/{id}')
+
+    with step("Сheck results"):
+        assert response.status_code == 200
+        assert S(get_booking.schema) == response.json()
 
 
 @allure.tag("api")
@@ -103,18 +115,21 @@ def test_get_booking(booker):
 def test_update_booking(booker):
     """Updates a current booking"""
 
-    id = create_new_bookingid(booker)
-    headers = BookingData.headers_data()
-    payload = BookingData.payload_data()
-    data = json.dumps(payload)
+    with step("Data preparation"):
+        id = create_new_bookingid(booker)
+        headers = BookingData.headers_data()
+        payload = BookingData.payload_data()
+        data = json.dumps(payload)
 
-    response: Response = booker.put(f'/booking/{id}', headers=headers, data=data)
+    with step("Response getting"):
+        response: Response = booker.put(f'/booking/{id}', headers=headers, data=data)
 
-    assert response.status_code == 200
-    assert S(get_booking.schema) == response.json()
-    assert payload.get("firstname") == response.json()["firstname"]
-    assert payload.get("lastname") == response.json()["lastname"]
-    assert payload.get("depositpaid") == response.json()["depositpaid"]
+    with step("Сheck results"):
+        assert response.status_code == 200
+        assert S(get_booking.schema) == response.json()
+        assert payload.get("firstname") == response.json()["firstname"]
+        assert payload.get("lastname") == response.json()["lastname"]
+        assert payload.get("depositpaid") == response.json()["depositpaid"]
 
 
 @allure.tag("api")
@@ -125,17 +140,20 @@ def test_update_booking(booker):
 def test_partial_update_booking(booker):
     """Updates a current booking with a partial payload"""
 
-    id = create_new_bookingid(booker)
-    headers = BookingData.headers_data()
-    payload = BookingData.payload_data_short()
-    data = json.dumps(payload)
+    with step("Data preparation"):
+        id = create_new_bookingid(booker)
+        headers = BookingData.headers_data()
+        payload = BookingData.payload_data_short()
+        data = json.dumps(payload)
 
-    response: Response = booker.patch(f'/booking/{id}', headers=headers, data=data)
+    with step("Response getting"):
+        response: Response = booker.patch(f'/booking/{id}', headers=headers, data=data)
 
-    assert response.status_code == 200
-    assert S(get_booking.schema) == response.json()
-    assert payload.get("firstname") == response.json()["firstname"]
-    assert payload.get("lastname") == response.json()["lastname"]
+    with step("Сheck results"):
+        assert response.status_code == 200
+        assert S(get_booking.schema) == response.json()
+        assert payload.get("firstname") == response.json()["firstname"]
+        assert payload.get("lastname") == response.json()["lastname"]
 
 
 @allure.tag("api")
@@ -146,10 +164,13 @@ def test_partial_update_booking(booker):
 def test_delete_booking(booker):
     """Deletes booking by the specified id"""
 
-    id = create_new_bookingid(booker)
-    headers = BookingData.headers_data()
+    with step("Data preparation"):
+        id = create_new_bookingid(booker)
+        headers = BookingData.headers_data()
 
-    response: Response = booker.delete(f'/booking/{id}', headers=headers)
+    with step("Response getting"):
+        response: Response = booker.delete(f'/booking/{id}', headers=headers)
 
-    assert response.status_code == 201
-    assert response.text == 'Created'
+    with step("Сheck results"):
+        assert response.status_code == 201
+        assert response.text == 'Created'
