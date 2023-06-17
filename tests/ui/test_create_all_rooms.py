87c942327f26eb96ad07314e53aa83d0830c89e0
evgenii klimashko <klimashko108@gmail.com>
2023-06-17 22:09:02 +0300

    Исправил стили, убрал или закоментил time.sleep

--- tests/ui/test_create_all_rooms.py
+++ tests/ui/test_create_all_rooms.py
@@ -7,15 +7,13 @@ from pages.create_room_page import CreateRoomPage
 
 def test_all_rooms_on_frontpage(setup_browser):
     types_room = ["Single", "Double", "Twin", "Family", "Suite"]
-
     create_room = CreateRoomPage()
+
     create_room.open(setup_browser)
     create_room.login_admin_panel()
-    if browser.element('.fa.fa-remove.roomDelete'):
-        print("Есть предустановленная комната")
-    time.sleep(2)
+    # time.sleep(2)
+
     create_room.remove_preset_rooms()
-    # time.sleep(5)
     for type_room in types_room:
         room_features = Room.room_features(type_room)
         room = Room(type_room=room_features.get('type_room'),
@@ -26,9 +24,8 @@ def test_all_rooms_on_frontpage(setup_browser):
                     refresh=room_features.get('refresh'),
                     safe=room_features.get('safe'),
                     views=room_features.get('views'))
-
         create_room.create_new_room(room=room)
-        # time.sleep(3)
-    time.sleep(2)
+    # time.sleep(3)
+
     create_room.go_to_frontpage()
     create_room.assert_all_rooms_on_frontpage(*types_room)
