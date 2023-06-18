15fc16b1787186c4b7ef985bbd574cccf781af7b
evgenii klimashko <klimashko108@gmail.com>
2023-06-18 03:54:39 +0300

    Закоментировал фикстуру browser_management

--- conftest.py
+++ conftest.py
@@ -23,21 +23,21 @@ def booker(allure_attachments_flag=1):
 DEFAULT_BROWSER_VERSION = "100.0"
 
 
-@pytest.fixture(scope='function', autouse=True)
-def browser_management():
-    browser.config.hold_browser_open = True
-    browser.config.window_width = '1024'
-    browser.config.window_height = '768'
-    browser.config.timeout = 3.0
-    browser.config.base_url = "https://automationintesting.online"
-
-    yield browser
-
-    attach_ui.add_html(browser)
-    attach_ui.add_screenshot(browser)
-    attach_ui.add_logs(browser)
-    attach_ui.add_video(browser)
-    browser.quit()
+# @pytest.fixture(scope='function', autouse=True)
+# def browser_management():
+#     browser.config.hold_browser_open = True
+#     browser.config.window_width = '1024'
+#     browser.config.window_height = '768'
+#     browser.config.timeout = 3.0
+#     browser.config.base_url = "https://automationintesting.online"
+#
+#     yield browser
+#
+#     attach_ui.add_html(browser)
+#     attach_ui.add_screenshot(browser)
+#     attach_ui.add_logs(browser)
+#     attach_ui.add_video(browser)
+#     browser.quit()
 
 
 def pytest_addoption(parser):
